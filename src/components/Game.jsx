6f72c978159f59e34789b611659c47b559aa91b4
Danmar Varela <vareladanmar@gmail.com>
2022-10-11 03:27:39 +0800

    Make chessboard responsive on small screen

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,7 +3,7 @@ import PlayerInfo from "./PlayerInfo";
 
 const Game = () => {
   return (
-    <div className="w-4/5 max-w-lg h-auto flex flex-col gap-3">
+    <div className="w-11/12 md:w-4/5 max-w-lg h-auto flex flex-col gap-3">
       <PlayerInfo name="Player1" />
       <Chessboard />
       <PlayerInfo name="Player2" />
