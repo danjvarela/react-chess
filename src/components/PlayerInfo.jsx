017c30b347957dfb35662d72a538a94b50e9d525
Fey Daniel <feysy97@gmail.com>
2022-10-17 18:19:57 +0800

    Add captured pieces

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,12 +1,41 @@
 import Stopwatch from "./Stopwatch";
 import Player from "./Player";
+import {useChess} from "contexts/chessContext";
+import pieces from "./pieces";
 
 const PlayerInfo = ({name}) => {
+  const {chess} = useChess();
+
   return (
-    <div className="flex justify-between items-start">
-      <Player name={name} />
-      <Stopwatch />
-    </div>
+    <>
+      <div className="flex justify-between items-start">
+        <Player name={name} />
+        <Stopwatch />
+      </div>
+      <div className="captured-pieces ml-8 -mt-6 flex">
+        {name === "Player2" &&
+          chess.history({verbose: true}).map(
+            ({captured, color, san}) =>
+              color === "w" && (
+                <div className="-ml-5" key={san}>
+                  {" "}
+                  {pieces[captured]}{" "}
+                </div>
+              )
+          )}
+
+        {name === "Player1" &&
+          chess.history({verbose: true}).map(
+            ({captured, color, san}) =>
+              color === "b" && (
+                <div className="-ml-5" key={san}>
+                  {" "}
+                  {pieces[captured?.toUpperCase()]}{" "}
+                </div>
+              )
+          )}
+      </div>
+    </>
   );
 };
 
