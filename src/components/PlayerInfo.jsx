3526579b4ce623e927b1e5758af66d21c89598dc
Fey Daniel <feysy97@gmail.com>
2022-10-17 20:41:07 +0800

    Add unicodes

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,39 +1,49 @@
 import Stopwatch from "./Stopwatch";
 import Player from "./Player";
 import {useChess} from "contexts/chessContext";
-import pieces from "./pieces";
 
 const PlayerInfo = ({name}) => {
   const {chess} = useChess();
 
+  const unicodes = {
+    K: "\u2654",
+    k: "\u2655",
+    Q: "\u2655",
+    q: "\u265B",
+    R: "\u2656",
+    r: "\u265C",
+    B: "\u2657",
+    b: "\u265D",
+    N: "\u2658",
+    n: "\u265E",
+    P: "\u2659",
+    p: "\u265F",
+  };
+
   return (
     <>
       <div className="flex justify-between items-start">
-        <Player name={name} />
-        <Stopwatch />
-      </div>
-      <div className="captured-pieces ml-8 -mt-6 flex">
-        {name === "Player2" &&
-          chess.history({verbose: true}).map(
-            ({captured, color, san}) =>
-              color === "w" && (
-                <div className="-ml-5" key={san}>
-                  {" "}
-                  {pieces[captured]}{" "}
-                </div>
-              )
-          )}
+        <div className="flex-col justify-center item-start">
+          <Player name={name} />
+          <div className="captured-pieces text-3xl flex">
+            {name === "Player2" &&
+              chess
+                .history({verbose: true})
+                .map(({captured, color, san}) =>
+                  color === "w" ? <div key={san}>{unicodes[captured]}</div> : null
+                )}
 
-        {name === "Player1" &&
-          chess.history({verbose: true}).map(
-            ({captured, color, san}) =>
-              color === "b" && (
-                <div className="-ml-5" key={san}>
-                  {" "}
-                  {pieces[captured?.toUpperCase()]}{" "}
-                </div>
-              )
-          )}
+            {name === "Player1" &&
+              chess.history({verbose: true}).map(({captured, color, san}) =>
+                color === "b" ? (
+                  <div className="text-white" key={san}>
+                    {unicodes[captured?.toUpperCase()]}
+                  </div>
+                ) : null
+              )}
+          </div>
+        </div>
+        <Stopwatch />
       </div>
     </>
   );
