0e706f505db4ead4f7c388f81cf95d1a7416552f
Fey Daniel <feysy97@gmail.com>
2022-10-19 19:17:40 +0800

    Update stopwatch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,7 +1,24 @@
-const Stopwatch = () => {
+import {useChess} from "contexts/chessContext";
+import {useCountdown} from "hooks/useCountdown";
+
+const Stopwatch = ({
+  targetTime,
+  //piece
+}) => {
+  const {chess} = useChess();
+  const [minutes, seconds] = useCountdown(targetTime);
   return (
-    <div className="prose prose-zinc dark:prose-invert prose-2xl bg-zinc-800 px-5 rounded-md">
-      1:45
+    <div
+      className={`${
+        targetTime ? "text-red-500" : ""
+      } prose prose-zinc dark:prose-invert prose-2xl bg-zinc-800 px-5 rounded-md`}
+    >
+      {targetTime
+        ? minutes + seconds <= 0
+          ? "0:00"
+          : `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+        : "1:45"}
+      {/* {minutes + seconds <= 0 && Object.assign(chess.turn(), piece)} */}
     </div>
   );
 };
